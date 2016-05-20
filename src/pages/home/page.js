292cfd44bc7520ed1e0a648247392d893bffc031
enric <enricx@yahoo.es>
2016-05-20 23:27:20 +0200

    Demo

--- src/pages/home/page.js
+++ src/pages/home/page.js
@@ -6,7 +6,7 @@ export default class HomePage extends React.Component {
   render() {
     return (
       <div className={styles.content}>
-        <h1>Home Page  ...,,</h1>
+        <h1>Home Page de l'Enric'Xuclà </h1>
         <p className={styles.welcomeText}>Thanks for joining!</p>
       </div>
     );
